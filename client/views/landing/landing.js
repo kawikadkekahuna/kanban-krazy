547c039e1abdffe422fdad2250d259c36b995bc7
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-07-31 11:46:21 +0000

    started registration process

--- client/views/landing/landing.js
+++ client/views/landing/landing.js
@@ -18,5 +18,23 @@ Template.landing.helpers({
 
 Template.landing.events({
 
+	'click .landing-button': function(event,template){
+		event.preventDefault();
+		console.log('button clicked')
+		var email = $('.landing-email').val();
+		var username = $('.landing-username').val();
+		var password = $('.landing-password').val();
+		console.log('email',email);
+		console.log('username',username);
+		console.log('password',password);
+		Accounts.createUser({
+			username: username,
+			password: password,
+			email: email,
+		},function(){
+			console.log('user added');
+		});
+	}
+
 });
 
